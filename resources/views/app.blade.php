ba7c243f83e29a5509943c1e2c8739f8d990e4aa
codewithme224 <codewithme224@gmail.com>
2024-05-05 21:36:48 +0000

    E-Hub Onboarding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet" />
+        <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+        </script>
 
         <!-- Scripts -->
         @routes
@@ -17,5 +21,28 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+
+
+        
+      
+
+<script>
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = ["red", "green","blue","orange","brown"];
+
+    new Chart("myChart", {
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    options: {...}
+    });
+</script>
+
     </body>
 </html>
